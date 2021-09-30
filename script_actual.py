1bba950b2d69a823ab130e53393a4548b3f54cac
DragonDare <deykohav@gmail.com>
2021-10-01 01:54:26 +0530

    First real commit

---
+++ script_actual.py
@@ -0,0 +1,8 @@
+import pyautogui
+
+scWidth, scHeight = pyautogui.size()    #Fetches monitor resolution
+scWidth, scHeight (1920,1080)   #Default monitor size
+
+cursorX, cursorY = pyautogui.position()    #Fetches XY coordinate of cursor
+cursorX, cursorY (1314,345)     #Default cursor position
+
